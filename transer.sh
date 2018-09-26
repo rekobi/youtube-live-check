454a33822b29cf5f98c75d9bd28abbdd96fb8f51
ZouWenbo <zouwb@ivymobi.com>
2018-09-26 10:50:14 +0800

    WIP

--- transer.sh
+++ transer.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
-result="$(node app.js)"
-no="no"
-if [ ${result} = ${no} ]; then
-echo "no live at now"
-else
-origin="$(youtube-dl --proxy socks5://127.0.0.1:1080 -g ${result})"
-echo ${origin}
-fi
+while [ 1 ]
+do
+  result="$(node app.js)"
+  no="no"
+  if [ ${result} = ${no} ]; then
+    echo "no live at now"
+    sleep 1m
+  else
+  origin="$(youtube-dl --proxy socks5://127.0.0.1:1080 -g ${result})"
+  echo ${origin}
+  sleep 1m
+  fi
+done
