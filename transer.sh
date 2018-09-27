54ef430d90958e145ad1d4a08cf73ba802f13266
ZouWenbo <zouwb@ivymobi.com>
2018-09-27 10:40:41 +0800

    WIP:格式！

--- transer.sh
+++ transer.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 while [ 1 ]
-do
-  result="$(node app.js)"
-  no="no"
-  if [ ${result} = ${no} ]; then
-    echo "no live at now"
-    sleep 1m
-  else
-  origin="$(youtube-dl -f "[height <=? 720][tbr>500]" -g ${result})"
-  streamCode=""
-  ffmpeg -i ${origin} -c:v copy -c:a aac -b:a 320k -ar 44100 -bufsize 1000k -strict -2 -f flv ${streamCode}
-  sleep 1m
-  fi
+  do
+    result="$(node app.js)"
+    no="no"
+    if [ ${result} = ${no} ]; then
+      echo "no live at now"
+      sleep 1m
+    else
+      origin="$(youtube-dl -f "[height <=? 720][tbr>500]" -g ${result})"
+      streamCode=""
+      ffmpeg -i ${origin} -c:v copy -c:a aac -b:a 320k -ar 44100 -bufsize 1000k -strict -2 -f flv ${streamCode}
+      sleep 1m
+    fi
 done
