676ddfae75d3b96c60dd897764d03f4ee4ed3993
ZouWenbo <zouwb@ivymobi.com>
2018-09-27 10:09:03 +0800

    WIP:添加了选择720P及以下并且码率>500的源

--- transer.sh
+++ transer.sh
@@ -7,7 +7,7 @@ do
     echo "no live at now"
     sleep 1m
   else
-  origin="$(youtube-dl -g ${result})"
+  origin="$(youtube-dl -f "[height <=? 720][tbr>500]" -g ${result})"
   streamCode=""
   ffmpeg -i ${origin} -c:v copy -c:a aac -b:a 320k -ar 44100 -strict -2 -f flv ${streamCode}
   sleep 1m
